c15cb97bde96dc07a66785cbfe4c62cd670b1028
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-21 00:16:18 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -31,6 +31,6 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 
-void (*AlerterTarget_FP[MAX_ALERTER_TYPE])(BreachType);
+//void (*AlerterTarget_FP[MAX_ALERTER_TYPE])(BreachType);
 
-void (*MailAlertType_FP[MAX_BREACH_TYPE])(BreachType);
+//void (*MailAlertType_FP[MAX_BREACH_TYPE])(BreachType);
