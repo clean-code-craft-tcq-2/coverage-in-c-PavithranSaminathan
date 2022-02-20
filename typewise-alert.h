1b1e60ec387c1029260f05a8e278f5e4c979a287
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-20 23:50:27 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,32 +1,33 @@
 #pragma once
 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
+#include "Alerter_cfg.h"
 
-typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
-} BreachType;
+//typedef enum {
+//  PASSIVE_COOLING,
+//  HI_ACTIVE_COOLING,
+//  MED_ACTIVE_COOLING
+//} CoolingType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType inferBreach(TempConfig ATempConfig, double double temperatureInC);
+//BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
+  MAX_ALERTER_TYPE
 } AlertTarget;
 
 typedef struct {
   CoolingType coolingType;
-  char brand[48];
+//  char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
+
+void (*AlerterTarget_FP)[MAX_ALERTER_TYPE](BreachType);
+
+void (*MailAlertType_FP)[MAX_BREACH_TYPE](BreachType);
