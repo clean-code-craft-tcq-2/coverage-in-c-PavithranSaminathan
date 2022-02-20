dddc63fee56a9b47f41ce4c0fd1b5a77404d9654
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-20 23:50:30 +0530

    Update Alerter_cfg.c

--- Alerter_cfg.c
+++ Alerter_cfg.c
@@ -1,5 +1,9 @@
 #include "Alerter_cfg.h"
 
+const char *AlerterMailContent[MAX_BREACH_TYPE]={"Hi, the temperature is too low\n",
+                                                 "Hi, the temperature is too high\n",
+                                                 "Hi, the temperature is Normal\n"
+                                                 };
 
 const struct TempConfig TempConfig_A[MAX_COOLING_TYPE]={
   {0,35},
