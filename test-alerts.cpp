e9d50a56a16f29315c2d0cb3e80388c28b089ba1
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-21 16:25:25 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,7 +3,18 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("Battery char belongs to PASSIVE COOLING -infers the breach according to limits")
+{
  BatteryCharacter batteryChar = {PASSIVE_COOLING};
   REQUIRE(inferBreach(batteryChar, 55) == TOO_HIGH);
+}
+TEST_CASE("Battery char belongs to PASSIVE COOLING -infers the breach according to limits") 
+{
+ BatteryCharacter batteryChar = {PASSIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, -10) == TOO_HIGH);
+}
+TEST_CASE("Battery char belongs to PASSIVE COOLING -infers the breach according to limits") 
+{
+ BatteryCharacter batteryChar = {PASSIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 10) == NORMAL);
 }
