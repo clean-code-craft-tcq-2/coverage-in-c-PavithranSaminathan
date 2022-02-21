645ec3d5291cd96248164b63cc65d607716d5603
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-21 21:05:01 +0530

    Update Alerter_cfg.c

--- Alerter_cfg.c
+++ Alerter_cfg.c
@@ -1,12 +1,17 @@
 #include "Alerter_cfg.h"
 
+/*configured alerter message content  */
+
 const char *AlerterMailContent[MAX_BREACH_TYPE]={"Hi, the temperature is too low\n",
                                                  "Hi, the temperature is too high\n",
                                                  "Hi, the temperature is Normal\n"
                                                  };
 
+
+/*Configuring temperature limit according to battery type*/
+
 const struct TempConfig TempConfig_A[MAX_COOLING_TYPE]={
-  {0,35},
-  {0,45},
-  {0,40}
+  {PASSIVE_COOLING_TEMP_LOWER_LIMIT    ,  PASSIVE_COOLING_TEMP_UPPER_LIMIT},
+  {HI_ACTIVE_COOLING_TEMP_LOWER_LIMIT  ,  HI_ACTIVE_COOLING_TEMP_UPPER_LIMIT},
+  {MID_ACTIVE_COOLING_TEMP_LOWER_LIMIT ,  MID_ACTIVE_COOLING_TEMP_UPPER_LIMIT}
 };
