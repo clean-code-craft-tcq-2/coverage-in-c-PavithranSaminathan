f3d68bc0b9e1f13cfbfc32c8c1ed3e7648f4b6d1
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-21 23:27:16 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,15 +3,20 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("Battery char belongs to PASSIVE COOLING -Too high")
+
+/*********************************************************************************************/
+/********** Battery Character belongs to  PASSIVE COOLING and different temperature level*****/
+/*********************************************************************************************/
+
+TEST_CASE("Battery character belongs to PASSIVE COOLING -Temperature breach high value")
 {
  BatteryCharacter batteryChar = {PASSIVE_COOLING};
-  REQUIRE(inferBreach(batteryChar, 55) == TOO_HIGH);
+  REQUIRE(inferBreach(batteryChar, 36) == TOO_HIGH);
 }
-TEST_CASE("Battery char belongs to PASSIVE COOLING -Too Low") 
+TEST_CASE("Battery char belongs to PASSIVE COOLING -Temperature is too low") 
 {
  BatteryCharacter batteryChar = {PASSIVE_COOLING};
-  REQUIRE(inferBreach(batteryChar, -10) == TOO_LOW);
+  REQUIRE(inferBreach(batteryChar, -1) == TOO_LOW);
 }
 TEST_CASE("Battery char belongs to PASSIVE COOLING -Normal") 
 {
@@ -19,15 +24,78 @@ TEST_CASE("Battery char belongs to PASSIVE COOLING -Normal")
   REQUIRE(inferBreach(batteryChar, 10) == NORMAL);
 }
 
+
+/*******************************************************************************************/
+/******** Battery Character belongs to  HI ACTIVE COOLING and different temperature level***/
+/*******************************************************************************************/
+
+TEST_CASE("Battery character belongs to HI ACTIVE COOLING -Temperature breach high value")
+{
+ BatteryCharacter batteryChar = {HI_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 46) == TOO_HIGH);
+}
+TEST_CASE("Battery char belongs to HI ACTIVE COOLING -Temperature is too low") 
+{
+ BatteryCharacter batteryChar = {HI_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, -1) == TOO_LOW);
+}
+TEST_CASE("Battery char belongs to HI ACTIVE COOLING  -NORMAL") 
+{
+ BatteryCharacter batteryChar = {HI_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 22) == NORMAL);
+}
+
+/********************************************************************************************/
+/****** Battery Character belongs to  MID ACTIVE COOLING and different temperature level*****/
+/********************************************************************************************/
+
+
+TEST_CASE("Battery character belongs to MID ACTIVE COOLING -Temperature breach high value")
+{
+ BatteryCharacter batteryChar = {MID_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 41) == TOO_HIGH);
+}
+TEST_CASE("Battery character belongs to MID ACTIVE COOLING -Temperature breach high value")
+{
+ BatteryCharacter batteryChar = {MID_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 40) == NORMAL);
+}
+TEST_CASE("Battery char belongs to MID ACTIVE COOLING -Temperature is too low") 
+{
+ BatteryCharacter batteryChar = {MID_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, -1) == TOO_LOW);
+}
+
+
+TEST_CASE("Battery char belongs to MID ACTIVE COOLING -Temperature is too low") 
+{
+ BatteryCharacter batteryChar = {MID_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 0) == NORMAL);
+}
+
+TEST_CASE("Battery char belongs to MID ACTIVE COOLING -Temperature in normal") 
+{
+ BatteryCharacter batteryChar = {MID_ACTIVE_COOLING};
+  REQUIRE(inferBreach(batteryChar, 22) == NORMAL);
+}
+
+/****************************************************/
+/********** TEST Alert message send to controller*****/
+/*****************************************************/
+
 TEST_CASE("CheckAndAlert-TOOHIGH -SENTTOCONTROLLER") 
 {
   BatteryCharacter batteryChar = {PASSIVE_COOLING};
   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,55) == SENTTOCONTROLLER);
 }
 
+
+/************************************************/
+/********** Test Alert message send to Email*****/
+/************************************************/
 TEST_CASE("CheckAndAlert-TOOHIGH -SENTTOEMAIL") 
 {
   BatteryCharacter batteryChar = {PASSIVE_COOLING};
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,55) == SENTTOEMAIL);
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,30) == SENTTOEMAIL);
 }
 
